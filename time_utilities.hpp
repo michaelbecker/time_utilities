36eb3aa0566d060c4f1103bdbfbb9b87f1f9d196
Michael Becker <michael.f.becker@gmail.com>
2016-02-20 21:15:16 -0500

    Adding CTimeVal class as wrapper for timeval

--- time_utilities.hpp
+++ time_utilities.hpp
@@ -40,8 +40,13 @@
 #ifndef TIME_UTILITIES_HPP__
 #define TIME_UTILITIES_HPP__
 
+
 #include <iostream>
 #include <ctime>
+
+#ifdef USING_TIMEVAL
+#include <sys/time.h>
+#endif
 
 
 /**
@@ -115,6 +120,28 @@ class CTimeSpec
             ts.tv_sec = sec;
             ts.tv_nsec = nsec;
         }
+
+#ifdef USING_TIMEVAL
+        /**
+         *  ctor - Create a CTimeSpec from a timeval struct.
+         *  @param tv timeval structure used to init the class.
+         *  This ctor guarantees that the structure is normalized.
+         */
+        CTimeSpec(struct timeval tv)
+        {
+            ts.tv_sec = tv.tv_sec;
+            ts.tv_nsec = tv.tv_usec * 1000;
+            
+            while (ts.tv_nsec >= NS_IN_SECOND) {
+                ts.tv_sec++;
+                ts.tv_nsec -= NS_IN_SECOND;
+            }
+            while (ts.tv_nsec < 0) {
+                ts.tv_sec--;
+                ts.tv_nsec += NS_IN_SECOND;
+            }
+        }
+#endif
 
         /**
          *  Static factory returning a CTimeSpec that represents "now"
@@ -315,6 +342,301 @@ class CTimeSpec
         return CTimeSpec(difference);
     }
 };
+
+
+/*
+ *  The struct timeval structure is not part of POSIX, however it is 
+ *  used a lot in Linux / BSD / *nix code, and the structure is very 
+ *  similar to the timespec structure. Define USING_TIMEVAL to get the 
+ *  corresponding APIs.
+ */
+#ifdef USING_TIMEVAL
+
+
+/**
+ *  Wrapper class for working with c style timeval structures.
+ */
+class CTimeVal 
+{
+    public:
+
+        /**
+         *  Default null ctor.
+         */
+        CTimeVal() 
+        : tv {0, 0}
+        {}
+
+        /**
+         *  ctor 
+         *  @param ms total milliseconds used to init the class.
+         */
+        CTimeVal(unsigned int ms) 
+        : tv {ms / MS_IN_SECOND, (ms % MS_IN_SECOND) * US_IN_MS}
+        {}
+
+        /**
+         *  ctor
+         *  @param t timeval structure used to init the class.
+         *  This ctor guarantees that the structure is normalized.
+         */
+        CTimeVal(struct timeval t)
+        {
+            while (t.tv_usec >= US_IN_SECOND) {
+                t.tv_sec++;
+                t.tv_usec -= US_IN_SECOND;
+            }
+            while (t.tv_usec < 0) {
+                t.tv_sec--;
+                t.tv_usec += US_IN_SECOND;
+            }
+
+            tv = t;
+        }
+
+        /**
+         *  ctor
+         *  @param sec seconds used to init the sec portion.
+         *  @param usec microseconds used to init the usec portion.
+         *  This ctor guarantees that the structure is normalized.
+         */
+        CTimeVal(time_t sec, long usec) 
+        {
+            while (usec >= US_IN_SECOND) {
+                sec++;
+                usec -= US_IN_SECOND;
+            }
+            while (usec < 0) {
+                sec--;
+                usec += US_IN_SECOND;
+            }
+
+            tv.tv_sec = sec;
+            tv.tv_usec = usec;
+        }
+
+        /**
+         *  ctor - create a CTimeVal class from a timespec struct.
+         *  @param t timeval structure used to init the class.
+         *  This ctor guarantees that the structure is normalized.
+         */
+        CTimeVal(struct timespec ts)
+        {
+            tv.tv_sec = ts.tv_sec;
+            tv.tv_usec = ts.tv_nsec / 1000;
+            
+            while (tv.tv_usec >= US_IN_SECOND) {
+                tv.tv_sec++;
+                tv.tv_usec -= US_IN_SECOND;
+            }
+            while (tv.tv_usec < 0) {
+                tv.tv_sec--;
+                tv.tv_usec += US_IN_SECOND;
+            }
+        }
+
+        /**
+         *  Static factory returning a CTimeVal that represents "now"
+         *  in wall clock time. See CLOCK_REALTIME.
+         */
+        static CTimeVal Now()
+        {
+            struct timespec ts;
+            clock_gettime(CLOCK_REALTIME, &ts);
+            return CTimeVal {ts};
+        }
+
+        /**
+         *  Static factory returning a CTimeVal that represents "now"
+         *  in monotonic time. See CLOCK_MONOTONIC.
+         */
+        static CTimeVal NowMonotonic()
+        {
+            struct timespec ts;
+            clock_gettime(CLOCK_MONOTONIC, &ts);
+            return CTimeVal {ts};
+        }
+
+        /**
+         *  Static factory returning a CTimeVal that represents "now"
+         *  in monotonic raw time. See CLOCK_MONOTONIC_RAW.
+         */
+        static CTimeVal NowMonotonicRaw()
+        {
+            struct timespec ts;
+            clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+            return CTimeVal {ts};
+        }
+
+        /**
+         *  Utility function to return a copy of the internal 
+         *  timeval structure.
+         */
+        struct timeval c_timeval()
+        {
+            return tv;
+        }
+
+        /**
+         *  Adds a CTimeVal to this one. 
+         *  Guarantees the result is normalized.
+         */
+        CTimeVal& operator+=(const CTimeVal& rhs)
+        {
+            tv.tv_sec += rhs.tv.tv_sec;
+            tv.tv_usec += rhs.tv.tv_usec;
+            if (tv.tv_usec >= US_IN_SECOND){
+                tv.tv_sec++;
+                tv.tv_usec -= US_IN_SECOND;
+            }
+            return *this;
+        }
+
+        /**
+         *  Subtracts a CTimeVal to this one. 
+         *  Guarantees the result is normalized.
+         */
+        CTimeVal& operator-=(const CTimeVal& rhs)
+        {
+            tv.tv_sec -= rhs.tv.tv_sec;
+            tv.tv_usec -= rhs.tv.tv_usec;
+            if (tv.tv_usec < 0){
+                tv.tv_sec--;
+                tv.tv_usec += US_IN_SECOND;
+            }
+            return *this;
+        }
+
+        /**
+         *  Compares a CTimeVals against this one 
+         *  for non-equality.
+         */
+        bool operator!=(const CTimeVal& rhs) const
+        {
+            if (tv.tv_sec != rhs.tv.tv_sec)
+                return true;
+            else if (tv.tv_usec != rhs.tv.tv_usec)
+                return true;
+            else 
+                return false;
+        }
+
+        /**
+         *  Compares a CTimeVals against this one 
+         *  for equality.
+         */
+        bool operator==(const CTimeVal& rhs) const
+        {
+            return !(*this != rhs);
+        }
+
+        /**
+         *  Compares a CTimeVals against this one 
+         *  for less than.
+         */
+        bool operator<(const CTimeVal& rhs) const 
+        {
+            if (tv.tv_sec < rhs.tv.tv_sec)
+                return true;
+            else if (tv.tv_usec < rhs.tv.tv_usec)
+                return true;
+            else 
+                return false;
+        }
+        
+        /**
+         *  Compares a CTimeVals against this one 
+         *  for greater than.
+         */
+        bool operator>(const CTimeVal& rhs) const 
+        {
+            if (tv.tv_sec > rhs.tv.tv_sec)
+                return true;
+            else if (tv.tv_usec > rhs.tv.tv_usec)
+                return true;
+            else 
+                return false;
+        }
+
+        /**
+         *  Compares a CTimeVals against this one 
+         *  for less than or equals to.
+         */
+        bool operator<=(const CTimeVal& rhs) const
+        {
+            if (*this < rhs)
+                return true;
+            else if (*this == rhs)
+                return true;
+            else 
+                return false;
+        }
+
+        /**
+         *  Compares a CTimeVals against this one 
+         *  for greater than or equals to.
+         */
+        bool operator>=(const CTimeVal& rhs) const
+        {
+            if (*this > rhs)
+                return true;
+            else if (*this == rhs)
+                return true;
+            else 
+                return false;
+        }
+
+    private:
+        /**
+         *  The internal data struct this class is wrapping.
+         */
+        struct timeval tv;
+
+    /**
+     *  Output operator for std::ostreams.
+     */
+    friend std::ostream& operator<< (std::ostream& os, const CTimeVal& tv)
+    {
+        os << "(" << tv.tv.tv_sec << " sec, " << tv.tv.tv_usec << " usec)";
+        return os;
+    }
+
+    /**
+     *  Adds two CTimeVals and returns a new one which is the sum.
+     */
+    friend CTimeVal operator+ (const CTimeVal& lhs, const CTimeVal& rhs)
+    {
+        struct timeval sum;
+        sum.tv_sec = lhs.tv.tv_sec + rhs.tv.tv_sec;
+        sum.tv_usec = lhs.tv.tv_usec + rhs.tv.tv_usec;
+        if (sum.tv_usec >= US_IN_SECOND){
+            sum.tv_sec++;
+            sum.tv_usec -= US_IN_SECOND;
+        }
+        
+        return CTimeVal(sum);
+    }
+
+    /**
+     *  Subtracts two CTimeVals and returns a new one which is the 
+     *  difference.
+     */
+    friend CTimeVal operator- (const CTimeVal& lhs, const CTimeVal& rhs)
+    {
+        struct timeval difference;
+        difference.tv_sec = lhs.tv.tv_sec - rhs.tv.tv_sec;
+        difference.tv_usec = lhs.tv.tv_usec - rhs.tv.tv_usec;
+        if (difference.tv_usec < 0){
+            difference.tv_sec--;
+            difference.tv_usec += US_IN_SECOND;
+        }
+        
+        return CTimeVal(difference);
+    }
+};
+
+
+#endif /* USING_TIMEVAL */
 
 
 #endif
